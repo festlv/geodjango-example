a5e18b7911899a8f7d82ddf8eb759b211ec96c30
Reinis Veips <reinis.veips@wot.lv>
2014-01-15 00:15:52 +0200

    Izveidoju vizualizacijas piemeru ar OpenLayers

--- rtu/telpas/views.py
+++ rtu/telpas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from vectorformats.Formats import Django, GeoJSON
+from django.utils.safestring import mark_safe
 
-# Create your views here.
+from telpas.models import Eka
+
+def index(request):
+    qs = Eka.objects.all()
+    djf = Django.Django(geodjango="kontura", properties=['lat', 'lon'])
+    geoj = GeoJSON.GeoJSON()
+    s = geoj.encode(djf.decode(qs))
+    data = {'geojson':  mark_safe(s)}
+
+    return render(request, 'telpas/index.html', data)
